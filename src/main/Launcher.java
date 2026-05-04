6fc69d71b9705119b2ed1cebde09805dc3f92737
RYZEN <yael.botero@uao.edu.co>
2026-05-04 11:16:49 -0500

    ----PRIMERA VERSIÓN DEFINITIVA-----

---
+++ src/main/Launcher.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+public class Launcher {
+    public static void main(String[] args) {
+        System.setProperty("prism.order", "sw");
+        System.setProperty("prism.verbose", "true");
+        // Esta línea es opcional pero ayuda si el error persiste:
+        System.setProperty("java.library.path", "C:/javafx-sdk-25.0.3/bin");
+        
+        MainApp.main(args);
+    }
+}
