e07a50aa4b24b5b57821cc1710a4b07ee7c6660a
RYZEN <yael.botero@uao.edu.co>
2026-05-23 12:03:03 -0500

    Implementación de un filtro a la imagen

---
+++ src/main/java/modelo/ImageFilter.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import javafx.scene.image.Image;
+
+
+public interface ImageFilter {
+
+
+    void setOriginalImage(Image image);
+
+    Image applyColorBlindFilter(ColorBlindType type);
+
+    boolean hasImage();
+}
